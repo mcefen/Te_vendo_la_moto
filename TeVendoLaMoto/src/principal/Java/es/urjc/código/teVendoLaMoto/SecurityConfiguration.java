574990b940f607a0e82633a9ea48bc1de2374097
mcefen <mcefen@users.noreply.github.com>
2020-05-24 18:17:40 +0200

    Update and rename TeVendoLaMoto/src/main/java/es/urjc/code/teVendoLaMoto/SecurityConfiguration.java to TeVendoLaMoto/src/principal/Java/es/urjc/código/teVendoLaMoto/SecurityConfiguration.java

--- TeVendoLaMoto/src/principal/Java/es/urjc/código/teVendoLaMoto/SecurityConfiguration.java
+++ TeVendoLaMoto/src/principal/Java/es/urjc/código/teVendoLaMoto/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package es.urjc.code.teVendoLaMoto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMeSecuritygurerimportar  org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity ;
+importar  org.springframework.security.config.annotation.web.builders.HttpSecurity ;
+importar  org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter ;importar  org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity ;
+importar  org.springframework.security.config.annotation.web.builders.HttpSecurity ;
+importar  org.springframework.security.config.annotation.web.configuration.WebSecurityConf;igurerAdapter;
 
-@Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuratio
+@EnableGlobalMethodSecurity(securedEnabled = f)aalse
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
